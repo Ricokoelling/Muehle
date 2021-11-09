dd1b872b9ee28ff4c64c5bda71447cc32c0168f9
Rico Kölling <rico.koelling@uni-jena.de>
2021-11-09 16:56:21 +0100

    testMuehle maybe finished

--- src/testMuehle.java
+++ src/testMuehle.java
@@ -3,19 +3,61 @@
 public class testMuehle extends Stones{
 
     private boolean muehle = false;
-    private int pos1;
-    private int pos2;
     private Stones[] player= new Stones[16];
 
     public testMuehle() {
+        muehle = test();
+    }
+    public boolean fNumb(int pos){
 
+        if(pos == 4 || pos == 7 || pos == 10 || pos == 13 || pos == 16 || pos == 19 || pos == 22) {
+            return false;
+        }
+        return true;
+    }
+    public int sNumb(int pos){
+        if(pos == 2 || pos == 5 || pos == 8){
+            return 1;
+        }
+        else
+        if(pos == 17 || pos == 20 ||pos == 23) {
+            return 2;
+        }
+        else
+            return 0;
     }
 
-    public void test() {
+    public boolean test() {
         player = getPlayer();
 
         for(int i= 0; i < 24 ; i++){
-
+            int pos1 = player[i].getPosition();
+            int pos2 = player[i + 1].getPosition();
+            if(pos1 + 1 == pos2 && fNumb(pos2)){
+                if(player[i].getPlayer() == player[i+1].getPlayer()){
+                    int pos3 = player[i+2].getPosition();
+                    int pos4 = player[i-1].getPosition();
+                    if(pos1 + 2 == pos3 && pos2 + 1 == pos3 && fNumb(pos3)  && player[i+2].getPlayer() == player[i].getPlayer()){
+                        return true;
+                    }
+                    else if(pos1 - 1 == pos4 && pos2 - 2 == pos4 && player[i-1].getPlayer() == player[i].getPlayer()){
+                            if(pos4 == 3 || pos4 == 6 || pos4 == 9 || pos4 == 12 || pos4 == 15 || pos4 == 18 || pos4 == 21){
+                                return true;
+                            }
+                    }
+                }
+            }else if(sNumb(pos1) == 1 || sNumb(pos1) == 2 ){
+                for(int j = i+1 ; j < 24 ;j++){
+                    if(sNumb(player[j].getPosition()) == sNumb(pos1) && player[i].getPlayer() == player[j].getPlayer()){
+                        for(int k = j+1 ; k < 24 ; k++) {
+                            if(sNumb(player[k].getPosition()) == sNumb(player[j].getPosition()) && player[i].getPlayer() == player[k].getPlayer()){
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
         }
+        return false;
     }
 }
